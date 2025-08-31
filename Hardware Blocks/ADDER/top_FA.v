2716eca0faf726c6889e40da8231a5aacc756c5e
Kefayet Ullah, Ph.D. <69049304+kefayet93@users.noreply.github.com>
2025-08-31 00:14:08 -0400

    Update top_FA.v

--- Hardware Blocks/ADDER/top_FA.v
+++ Hardware Blocks/ADDER/top_FA.v
@@ -1,3 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Kefayet Ullah, Ph.D. 
+// 
+// Create Date: 08/19/2025 04:40:34 PM
+// Design Name: Full Adder
+// Module Name: top_FA
+// Project Name: ADDER
+// Target Devices: XCZU28DR
+// Tool Versions: Vivado 2020.2
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module top_FA(a, b, cin, sum, cout);
      input [3:0]a, b;
 	 input c;
@@ -17,4 +38,5 @@ module top_FA(a, b, cin, sum, cout);
 		end
 		  cout = carry;
 	   end
+
 	   
