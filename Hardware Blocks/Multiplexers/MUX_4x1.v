ec8dc1cfac12f93e3fa63f99416f5d43b35f72c3
Kefayet Ullah, Ph.D. <69049304+kefayet93@users.noreply.github.com>
2026-02-09 15:12:02 -0500

    Add files via upload

---
+++ Hardware Blocks/Multiplexers/MUX_4x1.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/08/2026 03:02:28 PM
+// Design Name: 
+// Module Name: MUX_4x1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUX_4x1(in, sel, out);
+ input [3:0] in; 
+ input [1:0] sel;
+ output out;
+ 
+ assign out = in[sel];
+endmodule
+
+
+module MUX_16x1(in, sel, out);
+
+  input [15:0] in;
+  input [3:0] sel;
+  output out;
+  
+  wire [3:0] t;
+  
+  // Instantiating 4x1 MUX modules to represent a 16x1 MUX
+  MUX_4x1 M0(in[3:0], sel[1:0], t0);
+  MUX_4x1 M1(in[7:4], sel[1:0], t1);
+  MUX_4x1 M2(in[11:8], sel[1:0], t2);
+  MUX_4x1 M3(in[15:12], sel[1:0], t3);
+  
+  MUX_4x1 M4(t, sel[3:2], out);
+
+endmodule
+
