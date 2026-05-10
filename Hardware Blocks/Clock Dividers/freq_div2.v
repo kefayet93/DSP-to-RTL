1c51d8147a7e02d56fe64ffdd085b15a7ff486f1
Kefayet Ullah, Ph.D. <69049304+kefayet93@users.noreply.github.com>
2026-05-10 01:45:34 -0400

    Add files via upload

--- Hardware Blocks/Clock Dividers/freq_div2.v
+++ Hardware Blocks/Clock Dividers/freq_div2.v
@@ -1,14 +1,48 @@
-module freq_div2 (
-     
-	input clk, rst,
-	output reg f_2 
-);
-    always @(posedge clk)
-	  begin
-	     if(rst)
-		   f_2 <= 0;
-		 else
-		   f_2 <= ~f_2;
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/10/2026 01:37:45 AM
+// Design Name: 
+// Module Name: freq_div2_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module freq_div2_tb(
+
+    );
+    
+    reg clk, rst;
+    
+    freq_div2 dut(clk, rst, f_2);
+    
+    
+    initial
+      begin
+         {clk, rst} = 0;
       end
-     
+      
+      always #5 clk = ~clk;
+      
+      initial
+        begin
+          rst = 1'b1;
+          #10;
+          rst = 1'b0;
+          
+        end
+            
+    
 endmodule
