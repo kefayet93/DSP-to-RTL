78cfd0b7fddcd9e47370a8825739b1c50e974466
Kefayet Ullah, Ph.D. <69049304+kefayet93@users.noreply.github.com>
2026-05-10 01:54:43 -0400

    Add files via upload

--- Hardware Blocks/Clock Dividers/freq_div2_tb.v
+++ Hardware Blocks/Clock Dividers/freq_div2_tb.v
@@ -1,21 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/10/2026 01:37:45 AM
+// Design Name: 
+// Module Name: freq_div2_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module freq_div2_tb();
-
-   reg clk, rst;
-   
-   freq_div2_tb dut(clk, rst, f_2);
-   
-   initial 
+    
+    reg clk, rst;
+    
+    freq_div2 dut(clk, rst, f_2);
+    
+    
+    initial
       begin
-	    {clk, rst) = 0;
-	  end 
-	  
-	  always #5 clk = ~clk;
-	  
-	  initial
-	    begin
-		  rst = 1'b1;
-		  #10;
-		  rst = 1'b0;
-		end
-
+         {clk, rst} = 0;
+      end
+      
+      always #5 clk = ~clk;
+      
+      initial
+        begin
+          rst = 1'b1;
+          #10;
+          rst = 1'b0;
+          
+        end
+            
+    
 endmodule
