a40beb2fbc7e0a392c1ad94873368574caf4b3fd
Kefayet Ullah, Ph.D. <69049304+kefayet93@users.noreply.github.com>
2026-06-03 02:08:34 -0400

    Add files via upload

--- C Programming/Pointers/pointers_declaration.c
+++ C Programming/Pointers/pointers_declaration.c
@@ -3,11 +3,11 @@
 int main()
 
 {
-   int i=1;
-   int *p=&i;
-   int *q;
-   q = p;
-   *q = 5;
+   int i=1;      // assigning value "1" to variable "i"
+   int *p=&i;    // assigning the address taken by variable "i" to the pointer variable "p"
+   int *q;       // declaring integer pointer variable "q"
+   q = p;        // assigning the value taken by "p" to "q"
+   *q = 5;       // assigning value 5 to the location pointed by q
    printf("%d", *p);
 }
 
